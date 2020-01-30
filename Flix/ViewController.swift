13224ab011e96bd2a709f1f8d3e18b53caff1c03
Jose Alvarez Avina <j.alvarez22@live.com>
2020-01-29 21:11:11 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Jose Alvarez on 1/29/20.
+//  Copyright © 2020 Jose Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
