ec6262022c71b5950088f1464aa196f5607ceb28
Jose Alvarez Avina <j.alvarez22@live.com>
2020-02-06 20:33:14 -0800

    First Commit

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Jose Alvarez on 2/6/20.
+//  Copyright © 2020 Jose Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
